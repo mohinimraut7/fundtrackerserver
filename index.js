08045f0e22e6bb259c0f7320e84ea7d64f8e2262
mohinimraut7 <mohinimraut7@gmail.com>
2026-01-29 13:42:23 +0530

    cors changes

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const revenueRoutes = require("./routes/revenue");
 
 env.config();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*", // later frontend URL टाकू शकतेस
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
